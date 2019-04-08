cb40b5eafea916453a8d48d3e88357ccb582c01a
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-09 01:22:22 +0200

    Added children list to RootNode, as all nonterminal nodes will have children. CollectChildren adds children to a nodes' child list, or to a desired list.

--- src/ASTVisitor/structure/RootNode.java
+++ src/ASTVisitor/structure/RootNode.java
@@ -1,14 +1,57 @@
 package ASTVisitor.structure;
 
+import ASTVisitor.ASTVisitor;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RuleContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 enum TYPE {INT, DOUBLE, STRING, TIME}
 
-public abstract class RootNode implements Node {
+public abstract class RootNode extends ASTVisitor implements Node {
+    public RootNode parent;
+    public ArrayList<RootNode> children = new ArrayList<>();
+
     public RootNode() { }
+
+    public RootNode(RootNode parent) {
+        this.parent = parent;
+    }
 
     @Override
     public String toPrettyString() {
         return null;
+    }
+
+    /**
+     * Collects children from a given list of type T extending ParserRulerContext to
+     * the this node's childen and adds this as parent to its children.
+     * @param list to collect from
+     * @param <T> type of list to collect from
+     */
+    public <T extends ParserRuleContext> void collectChildren(List<T> list) {
+        if(!list.isEmpty())
+            list.forEach(e -> children.add(super.visit(e)));
+        for(RootNode n : children)
+            if(n != null)
+                n.parent = this;
+    }
+    /**
+     * Collects children from a given list of type T extending ParserRuleContext to the supplied
+     * list. Adds this node as parent.
+     * @param source to collect from
+     * @param target to collect to
+     * @param <T> type of list to collect from
+     */
+    public <T extends ParserRuleContext> void collectChildren(List<T> source, ArrayList<RootNode> target) {
+        if(!source.isEmpty())
+            source.forEach(e -> target.add(super.visit(e)));
+        for(RootNode n : target) {
+            if (n != null)
+                n.parent = this;
+        }
     }
 }

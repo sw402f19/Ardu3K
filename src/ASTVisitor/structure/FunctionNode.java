470f70506bf314d65205fceb47634822fb9108da
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-09 12:43:30 +0200

    Added local block node.

--- src/ASTVisitor/structure/FunctionNode.java
+++ src/ASTVisitor/structure/FunctionNode.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class FunctionNode extends RootNode {
     public RootNode id;
     public RootNode parameter;
+    public RootNode block;
 
     @Override
     public String toString() {

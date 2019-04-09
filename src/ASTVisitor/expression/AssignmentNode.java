b1c9155063d2d0cc5a5739f09f15b76bb6d7db8b
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-09 13:15:07 +0200

    Added toString override.

--- src/ASTVisitor/expression/AssignmentNode.java
+++ src/ASTVisitor/expression/AssignmentNode.java
@@ -5,4 +5,9 @@ import ASTVisitor.structure.RootNode;
 public class AssignmentNode extends AbstractExpressionNode {
     public RootNode left;
     public RootNode right;
+
+    @Override
+    public String toString() {
+        return left.toString() +" = "+ right.toString();
+    }
 }

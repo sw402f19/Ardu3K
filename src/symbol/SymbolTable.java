e9d5c67e500bac8e3d3ffe75b8426cc94d24bd2a
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-14 06:39:26 +0200

    Close scope if of type block, adds new arraylist to scope display on open. Enter symbol adds symbol to scopeDisplay and symTable. Setup and loop nodes will also open scope.

--- src/symbol/SymbolTable.java
+++ src/symbol/SymbolTable.java
@@ -3,17 +3,17 @@ package symbol;
 import node.RootNode;
 import node.expression.AssignmentNode;
 import node.structure.BlockNode;
+import node.structure.LoopNode;
+import node.structure.SetupNode;
 import symbol.hashtable.HashTable;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 
 public class SymbolTable {
 
     private static SymbolTable ourInstance = new SymbolTable();
     private static final HashMap<Class, Handler> dispatch = new HashMap<>();
-    private static HashTable<Integer, Symbol> symTable = new HashTable<>();
+    private static HashTable<String, Symbol> symTable = new HashTable<>();
     private static ArrayList<ArrayList<Symbol>> scopeDisplay = new ArrayList<>();
     private static int depth = 0;
 
@@ -28,28 +28,67 @@ public class SymbolTable {
             handle(node);
             for (RootNode child : node.children)
                 processNode(child);
+            if(node instanceof BlockNode)
+                closeScope();
         }
-
     }
     private static void openScope() {
         depth++;
-        if(scopeDisplay.size() < depth)
-            scopeDisplay.add(null);
+        if(scopeDisplay.size() <= depth)
+            scopeDisplay.add(new ArrayList<>());
         else
-            scopeDisplay.set(depth, null);
+            scopeDisplay.set(depth-1, null);
     }
+    private static void closeScope() {
+        Symbol prevSym;
+        for(Symbol sym : scopeDisplay.get(depth-1)) {
+            prevSym = symTable.getNext(sym.getName().toString());
+            symTable.remove(sym.getName().toString());
+            if(prevSym != null)
+                symTable.add(prevSym.getName().toString(), prevSym);
+        }
+        depth--;
+    }
+    private static void enterSymbol(Symbol newSymbol) {
+        Symbol oldsymbol = retrieveSymbol(newSymbol);
+        if(oldsymbol != null && oldsymbol.getDepth() == depth)
+            error("Duplicate identifier of name: "+newSymbol.getName());
+
+        scopeDisplay.get(depth-1).add(newSymbol);
+        symTable.add(newSymbol.getName().toString(), newSymbol);
+    }
+
+    private static Symbol retrieveSymbol(Symbol name) {
+        Symbol symbol = symTable.get(name.getName().toString());
+        while (symbol != null) {
+            if (symbol.getName() == name.getName())
+                return symbol;
+            symbol = symbol.getHash();
+        }
+        return null;
+    }
+
     static {
         dispatch.put(AssignmentNode.class, n ->
                 handleAssignment((AssignmentNode) n));
         dispatch.put(BlockNode.class, n ->
                 handleBlock((BlockNode) n));
+        dispatch.put(SetupNode.class, n ->
+                handleSetup((SetupNode) n));
+        dispatch.put(LoopNode.class, n ->
+                handleLoop((LoopNode) n));
     }
 
     private static void handleAssignment(AssignmentNode n) {
-        symTable.add(n.hashCode(), new Symbol(n.getLeft(), n.getRight(), depth));
-        symTable.add(n.hashCode(), new Symbol(n.getLeft(), n.getRight(), depth));
+        enterSymbol(new Symbol(n.getLeft(), n.getRight(), depth));
     }
     private static void handleBlock(BlockNode node) {
+        openScope();
+    }
+    private static void handleSetup(SetupNode node) {
+        openScope();
+    }
+    private static void handleLoop(LoopNode node) {
         openScope();
     }
 
@@ -61,6 +100,9 @@ public class SymbolTable {
     }
     public static SymbolTable getInstance() {
         return ourInstance;
+    }
+    public static void error(String str) {
+        System.out.println(str);
     }
 
 

69127edf8d5d01ca51ff8152413e147193dfd013
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-14 17:01:41 +0200

    Added local level field.

--- src/symbol/Symbol.java
+++ src/symbol/Symbol.java
@@ -2,6 +2,8 @@ package symbol;
 
 import node.RootNode;
 
+import java.util.ArrayList;
+
 import static java.util.Objects.hash;
 
 public class Symbol {
@@ -10,6 +12,7 @@ public class Symbol {
     private Symbol var;
     private int depth;
     private Symbol hash;
+    private ArrayList<Symbol> level;
 
 
     public Symbol(RootNode name, RootNode type, int depth) {
@@ -34,11 +37,20 @@ public class Symbol {
     public Symbol getHash() {
         return hash;
     }
+
+    public ArrayList<Symbol> getLevel() {
+        return level;
+    }
+
     public void setVar(Symbol var) {
         this.var = var;
     }
     public void setHash(Symbol hash) {
         this.hash = hash;
+    }
+
+    public void setLevel(ArrayList<Symbol> level) {
+        this.level = level;
     }
 
     @Override

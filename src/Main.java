4c47c7df90f7093dcbb44b92617e4669ed4b927f
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-04-18 13:55:36 +0200

    Rename of BaseVisitor.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import node.RootNode;
-import symbol.SymbolTable;
+import symbol.OldSymbolTable;
 import visitor.BuildASTVisitor;
 import gen.Ardu3kLexer;
 import gen.Ardu3kParser;
@@ -22,7 +22,6 @@ public class Main {
 
             Ardu3kParser.CompileUnitContext cst = parser.compileUnit();
             ast =  new BuildASTVisitor().visitCompileUnit(cst);
-            SymbolTable.getInstance().buildSymbolTable(ast);
             //System.out.println("Im here");
 
         } catch (Exception e) {

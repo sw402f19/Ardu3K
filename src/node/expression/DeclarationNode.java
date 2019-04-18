8a7a2346c57165ff4d4214bdfe7d178edd926522
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-18 18:51:51 +0200

    Declaration node for when an assignment node's identifier is reached for the first time.

--- src/node/expression/DeclarationNode.java
+++ src/node/expression/DeclarationNode.java
@@ -2,6 +2,11 @@ package node.expression;
 
 public class DeclarationNode extends AbstractInfixExpressionNode {
 
+    public DeclarationNode(AbstractInfixExpressionNode node) {
+        this.setLeft(node.getLeft());
+        this.setRight(node.getRight());
+    }
+
     public String getName() {
         return getLeft().toString();
     }

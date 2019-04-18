7395b138590974c3e7e39d3a8b72359dc1ac8030
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-18 18:54:24 +0200

    Proper type parameter and initial visitAssignment/DeclarationNode methods.

--- src/visitor/SemanticsVisitor.java
+++ src/visitor/SemanticsVisitor.java
@@ -1,9 +1,29 @@
 package visitor;
 
 import node.RootNode;
+import node.expression.AssignmentNode;
+import node.expression.DeclarationNode;
+import symbol.SymbolTable;
 
-public class SemanticsVisitor<T> extends BaseASTVisitor<T> {
+public class SemanticsVisitor extends BaseASTVisitor<RootNode> {
 
-    /* Sec. 8.8 */
+    private SymbolTable symbolTable = SymbolTable.getInstance();
 
+    @Override
+    public RootNode visitAssignmentNode(AssignmentNode node) {
+        if(symbolTable.retrieveSymbol(node) == null)
+            return visitDeclarationNode(new DeclarationNode(node));
+        else
+            node.getRight().accept(new TypeVisitor());
+
+        return node;
+    }
+
+    @Override
+    public RootNode visitDeclarationNode(DeclarationNode node) {
+        symbolTable.enterSymbol(node);
+        new TypeVisitor().visitDeclarationNode(node);
+
+        return node;
+    }
 }
--- src/visitor/TypeVisitor.java
+++ src/visitor/TypeVisitor.java
@@ -1,9 +1,15 @@
 package visitor;
 
 import node.RootNode;
+import node.expression.DeclarationNode;
 
-public class TypeVisitor<T> extends TopDeclVisitor<T>{
+public class TypeVisitor extends TopDeclVisitor<RootNode>{
 
     /* Sec 8.6.2 (p304) + 8.6.5 (p311) + 8.6.6 (p312) + 8.6.7 (p313) */
 
+
+    @Override
+    public RootNode visitDeclarationNode(DeclarationNode node) {
+        return null;
+    }
 }

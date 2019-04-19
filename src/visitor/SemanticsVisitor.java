72041bc5d4e0f329b29fc7dc99eca000b6f2b345
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-04-19 14:13:28 +0200

    Added visitProgramNode(), which first visits defines, then functions, setup, and lastly loop.

--- src/visitor/SemanticsVisitor.java
+++ src/visitor/SemanticsVisitor.java
@@ -5,6 +5,11 @@ import node.expression.AbstractExpressionNode;
 import node.expression.AssignmentNode;
 import node.expression.DeclarationNode;
 import node.primary.IdentifierNode;
+import node.statement.ElifNode;
+import node.statement.ForNode;
+import node.statement.IfNode;
+import node.statement.SwitchNode;
+import node.structure.*;
 import symbol.SymbolTable;
 
 public class SemanticsVisitor extends BaseASTVisitor<RootNode> {
@@ -17,8 +22,7 @@ public class SemanticsVisitor extends BaseASTVisitor<RootNode> {
             return visitDeclarationNode(new DeclarationNode(node));
         else {
             if(!visitAbstractExpressionNode((AbstractExpressionNode) node.getRight()).getClass().equals(
-                    symbolTable.retrieveSymbol(node).getType())
-            )
+                    symbolTable.retrieveSymbol(node).getType()))
                 //todo temporary error handling
                 System.out.println("Imcompatible types");
         }
@@ -36,7 +40,7 @@ public class SemanticsVisitor extends BaseASTVisitor<RootNode> {
 
     // todo temporary error handling
     @Override
-    public RootNode visitIdentifier(IdentifierNode node) {
+    public RootNode visitIdentifierNode(IdentifierNode node) {
         if(symbolTable.retrieveSymbol(node) == null)
             System.out.println("Identifier not declared");
         else
@@ -47,6 +51,73 @@ public class SemanticsVisitor extends BaseASTVisitor<RootNode> {
     @Override
     public RootNode visitAbstractExpressionNode(AbstractExpressionNode node) {
         return super.visitAbstractExpressionNode(node);
+    }
+
+    @Override
+    public RootNode visitProgramNode(ProgramNode node) {
+        visit(node.getDefinesNode());
+        visit(node.getFunctionsNode());
+        visit(node.getSetupNode());
+        visit(node.getLoopNode());
+        return node;
+    }
+    public RootNode visitSetupNode(SetupNode node) {
+        symbolTable.openScope();
+        // todo typecheck children
+        return node;
+    }
+
+    @Override
+    public RootNode visitDefinesNode(DefinesNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        return node;
+    }
+
+    @Override
+    public RootNode visitDefineNode(DefineNode node) {
+        // todo typecheck children
+        return node;
+    }
+
+    @Override
+    public RootNode visitLoopNode(LoopNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        symbolTable.closeScope();
+        return node;
+    }
+
+    @Override
+    public RootNode visitForNode(ForNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        symbolTable.closeScope();
+        return node;
+    }
+
+    @Override
+    public RootNode visitSwitchNode(SwitchNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        symbolTable.closeScope();
+        return node;
+    }
+
+    @Override
+    public RootNode visitIfNode(IfNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        symbolTable.closeScope();
+        return node;
+    }
+
+    @Override
+    public RootNode visitElifNode(ElifNode node) {
+        symbolTable.openScope();
+        super.visit(node);
+        symbolTable.closeScope();
+        return node;
     }
 
 }

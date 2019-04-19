7496bbf28be8b06e7ce19417b8a09a2770aa9803
gamped <mathiasgam@gmail.com>
2019-04-19 10:03:24 +0200

    Written pseudocode for var decl

--- src/visitor/TypeVisitor.java
+++ src/visitor/TypeVisitor.java
@@ -5,7 +5,7 @@ import node.expression.DeclarationNode;
 import symbol.*;
 
 /* Sec 8.6.2 (p304) + 8.6.5 (p311) + 8.6.6 (p312) + 8.6.7 (p313) */
-public class TypeVisitor extends TopDeclVisitor<RootNode>{
+public class TypeVisitor extends TopDeclVisitor{
 
     /* === MOCK === */
     private SymbolTable st; // TODO: I'm not sure where we want to get the one we will use?
@@ -19,9 +19,9 @@ public class TypeVisitor extends TopDeclVisitor<RootNode>{
             // node.type = attr; // TODO: No type variable in RootNode
             // node.attributes = attr; // TODO: No attribute variable in nodes
         } else {
-            //node.type = errorNode; // TODO: I assume we want to make our own version instead of using ANTLER's?
+            // node.type = errorNode; // TODO: I assume we want to make our own version instead of using ANTLER's?
             // node.attributes = null; // TODO: No attribute variable in nodes
-            throw new Exception("This identifier is not a type name: " + node.name);
+            // throw new Exception("This identifier is not a type name: " + node.name);
         }
         return node;
     }

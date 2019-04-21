acc54d86a387775eaf6998154888a51b14b7f6e1
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-22 00:34:25 +0200

    Implement BooleanType.

--- src/node/expression/condition/AbstractInfixConditionalNode.java
+++ src/node/expression/condition/AbstractInfixConditionalNode.java
@@ -1,8 +1,10 @@
 package node.expression.condition;
 
 import node.expression.AbstractInfixExpressionNode;
+import node.expression.type.BooleanType;
 
-public abstract class AbstractInfixConditionalNode extends AbstractInfixExpressionNode {
+public abstract class AbstractInfixConditionalNode extends AbstractInfixExpressionNode
+        implements BooleanType {
 
     public AbstractInfixConditionalNode(){
 

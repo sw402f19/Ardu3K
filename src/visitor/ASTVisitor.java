145447bbb43b47c45810108d9b67a440f8536cf9
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-23 18:29:35 +0200

    Removed unnecessary imports.

--- src/visitor/ASTVisitor.java
+++ src/visitor/ASTVisitor.java
@@ -2,18 +2,6 @@ package visitor;
 
 import node.Node;
 import node.RootNode;
-import node.composite.ListNode;
-import node.expression.*;
-import node.expression.additive.AbstractInfixAdditiveNode;
-import node.expression.additive.MinusNode;
-import node.expression.additive.PlusNode;
-import node.expression.condition.*;
-import node.expression.multiplicative.*;
-import node.expression.relation.*;
-import node.expression.unary.UnaryNode;
-import node.primary.*;
-import node.statement.*;
-import node.scope.*;
 
 import java.util.ArrayList;
 

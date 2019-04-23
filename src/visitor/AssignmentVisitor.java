18370001793bb5d508690d3600277cd1bfe44fd8
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-23 18:05:53 +0200

    Compares expected type to primary node's type.

--- src/visitor/AssignmentVisitor.java
+++ src/visitor/AssignmentVisitor.java
@@ -1,8 +1,12 @@
 package visitor;
 
 import node.RootNode;
+import node.expression.AbstractExpressionNode;
+import node.expression.AbstractInfixExpressionNode;
 import node.expression.AssignmentNode;
 import node.expression.type.IllegalTypeException;
+import node.primary.AbstractPrimaryNode;
+import node.primary.IdentifierNode;
 import symbol.SymbolTable;
 
 public class AssignmentVisitor extends BaseASTVisitor<RootNode> {
@@ -21,10 +25,17 @@ public class AssignmentVisitor extends BaseASTVisitor<RootNode> {
         else
             throw new IllegalTypeException(node.getLine()+" Illegal type: "+expressionTree.toString()+" for "+node.toString());
     }
-
+    public RootNode visit(AbstractPrimaryNode node) throws IllegalTypeException {
+        if(!isInstanceOf(expectedType.getClass(), node))
+            throw new IllegalTypeException(
+                    node.getLine()+" Incompatible types "+node.getClass().getSimpleName()+", expected "+expectedType.getClass().getSimpleName());
+        return node;
+    }
+    public RootNode visit(IdentifierNode node) {
+        return symbolTable.retrieveSymbol(node).getType();
+    }
 
     public boolean isInstanceOf(Class clazz, Object obj) {
         return clazz.isInstance(obj);
     }
-
 }

a1fada65a9d2ca746b674f7d7350c6729632a6da
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-23 18:28:36 +0200

    Removed accept() to comply with Reflective Visitor Pattern.

--- src/node/primary/ListElement.java
+++ src/node/primary/ListElement.java
@@ -7,10 +7,4 @@ public class ListElement extends AbstractPrimaryNode {
 
     @Override
     public String toString() { return "List element"; }
-
-    @Override
-    public <T> T accept(ASTVisitor<? extends T> visitor) {
-        if ( visitor instanceof BaseASTVisitor) return ((BaseASTVisitor<? extends T>)visitor).visitListElement(this);
-        else return visitor.visitChildren(this);
-    }
 }

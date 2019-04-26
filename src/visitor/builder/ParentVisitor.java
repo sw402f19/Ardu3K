dfe40407298c4f3f95b6bc1168282b02a69df1c5
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-26 22:10:43 +0200

    Visitor to decorate nodes with reference to their parent. Will be used when typecasting.

---
+++ src/visitor/builder/ParentVisitor.java
@@ -0,0 +1,16 @@
+package visitor.builder;
+
+import node.Node;
+import node.RootNode;
+import visitor.BaseASTVisitor;
+
+public class ParentVisitor extends BaseASTVisitor<RootNode> {
+
+    public RootNode visit(RootNode node) {
+        for(RootNode n : node.children)
+            if(n != null)
+                n.parent = node;
+        visitChildren(node);
+        return node;
+    }
+}

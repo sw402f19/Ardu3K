4f31788099bab7258b982a0ba4160d8da4dda54c
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-28 01:13:13 +0200

    Static type caster which throws IllegalTypeException if no cast is available.

--- src/visitor/semantic/typecast/TypeCaster.java
+++ src/visitor/semantic/typecast/TypeCaster.java
@@ -1,6 +1,6 @@
 package visitor.semantic.typecast;
 
-import exception.ErrorNode;
+import exception.IllegalTypeException;
 import node.RootNode;
 import node.primary.IntegerNode;
 import node.primary.FloatNode;
@@ -11,43 +11,47 @@ import java.util.List;
 
 public class TypeCaster {
 
-    private static HashMap<Class, List<Class>> dispatch = new HashMap<>();
+    private static HashMap<Class, List<Class>> castable = new HashMap<>();
     static {
-        dispatch.put(IntegerNode.class, List.of(FloatNode.class, StringNode.class));
-        dispatch.put(FloatNode.class, List.of(FloatNode.class, StringNode.class));
+        castable.put(IntegerNode.class, List.of(FloatNode.class, StringNode.class));
+        castable.put(FloatNode.class, List.of(FloatNode.class, StringNode.class));
+        castable.put(StringNode.class, List.of());
     }
-    public static boolean canCast(RootNode source, RootNode target) {
-        return dispatch.get(source.getClass()).contains(target.getClass());
+    private static boolean canCast(RootNode source, RootNode target) {
+        return castable.get(source.getClass()).contains(target.getClass());
     }
-    public static RootNode cast(RootNode source, RootNode target) {
-        return handle(source, target.getClass());
+    public static RootNode cast(RootNode source, RootNode target) throws IllegalTypeException {
+        if (canCast(source, target))
+            return handle(source, target.getClass());
+        else throw new IllegalTypeException(source.getLine()+" Incompatible types, cannot cast "+source.toString()+
+                    " to "+target.toString());
     }
 
-    private static HashMap<Class, Handler> dispatch2 = new HashMap<>();
+    private static HashMap<Class, Handler> dispatch = new HashMap<>();
     static {
-        dispatch2.put(IntegerNode.class, (n, c) -> handleInteger((IntegerNode) n,c));
-        dispatch2.put(FloatNode.class, (n, c) -> handleReal((FloatNode)n, c));
+        dispatch.put(IntegerNode.class, (n, c) -> handleInteger((IntegerNode) n,c));
+        dispatch.put(FloatNode.class, (n, c) -> handleReal((FloatNode)n, c));
     }
     // todo temp errornode
     private static RootNode handleInteger(IntegerNode node, Class clazz) {
         if(clazz.isInstance(StringNode.class))
             return new StringNode(node.value);
-        else if(clazz.isInstance(FloatNode.class))
+        else
             return new FloatNode(node.value);
-        else return new ErrorNode(clazz);
     }
 
     private static RootNode handleReal(FloatNode node, Class clazz) {
         if(clazz.isInstance(StringNode.class))
             return new StringNode(node.value);
-        else if(clazz.isInstance(FloatNode.class))
+        else
             return new FloatNode(node.value);
-        else return new ErrorNode(clazz);
     }
-    private static RootNode handle(Object o, Class target) {
-        Handler h = dispatch2.get(o.getClass());
-        if(h != null)
+    private static RootNode handle(Object o, Class target) throws IllegalTypeException {
+        Handler h = dispatch.get(o.getClass());
+        if (h != null)
             return h.handle(o, target);
-        return null;
+        else
+            throw new IllegalTypeException();
+
     }
 }

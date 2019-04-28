be52e7208157ce352854afd927ef881cac661a1a
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2019-04-28 20:58:14 +0200

    Corrected node hierarchy.

--- src/node/expression/relation/AbstractInfixRelationNode.java
+++ src/node/expression/relation/AbstractInfixRelationNode.java
@@ -1,12 +1,13 @@
 package node.expression.relation;
 
 import node.RootNode;
+import node.expression.AbstractInfixBooleanNode;
 import node.expression.AbstractInfixExpressionNode;
 import node.expression.AbstractInfixNumeralNode;
 import node.expression.type.BooleanType;
 import org.antlr.v4.runtime.ParserRuleContext;
 
-public abstract class AbstractInfixRelationNode extends AbstractInfixNumeralNode {
+public abstract class AbstractInfixRelationNode extends AbstractInfixBooleanNode {
 
     public AbstractInfixRelationNode(RootNode parent) {
         super(parent);

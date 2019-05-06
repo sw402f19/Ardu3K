eb1636988d184b3d4205d3da3f8e32aae85a4232
kristian <kh.mikkelsen@hotmail.com>
2019-05-06 13:27:31 +0200

    Define, block, strings, integers.

--- src/visitor/codegen/CodeGenVisitor.java
+++ src/visitor/codegen/CodeGenVisitor.java
@@ -1,11 +1,16 @@
 package visitor.codegen;
 
 import exception.factory.SemanticException;
-import node.scope.DefinesNode;
-import node.scope.ProgramNode;
+import node.primary.BoolNode;
+import node.primary.IntegerNode;
+import node.primary.StringNode;
+import node.scope.*;
+import node.statement.control.ForNode;
 import visitor.BaseASTVisitor;
 
 public class CodeGenVisitor extends BaseASTVisitor<Void> {
+
+    OutBuilder outBuilder = OutBuilder.getInstance();
 
     public void visit(ProgramNode node) throws SemanticException {
         if(node.getDefinesNode() != null)
@@ -17,7 +22,31 @@ public class CodeGenVisitor extends BaseASTVisitor<Void> {
         if(node.getLoopNode() != null)
             visit(node.getLoopNode());
     }
-    public void visit(DefinesNode node) {
-
+    public void visit(DefineNode node) throws SemanticException {
+        outBuilder.append("#define ");
+        visitChildren(node);
+        outBuilder.newLine();
+    }
+    public void visit(IntegerNode node) {
+        outBuilder.append(""+node.value);
+    }
+    public void visit(StringNode node) {
+        outBuilder.append("\""+node.value+"\"");
+    }
+    public void visit(BoolNode node) {
+        outBuilder.append(""+node.value);
+    }
+    public void visit(SetupNode node) throws SemanticException {
+        outBuilder.append("void setup()");
+        visitChildren(node);
+    }
+    public void visit(BlockNode node) throws SemanticException {
+        outBuilder.append("{\n");
+        visitChildren(node);
+        outBuilder.append("}\n");
+    }
+    public void visit(ForNode node) throws SemanticException {
+        outBuilder.append("for("+node.getExpression()+" to "+node.getValue()+")");
+        visit(node.getStmt());
     }
 }

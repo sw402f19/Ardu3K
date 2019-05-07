af54c42a54c7bd8932ecc11a6c85d6d9fca83a9d
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-05-07 14:50:33 +0200

    Now properly returns the type of an expression.

--- src/visitor/semantic/ExpressionTypeVisitor.java
+++ src/visitor/semantic/ExpressionTypeVisitor.java
@@ -5,35 +5,39 @@ import node.RootNode;
 import node.composite.ListNode;
 import node.expression.AbstractInfixExpressionNode;
 import node.expression.additive.AbstractInfixAdditiveNode;
+import node.expression.additive.PlusNode;
 import node.expression.condition.*;
 import node.expression.multiplicative.*;
 import node.expression.relation.*;
 import node.expression.type.BooleanType;
 import exception.type.IllegalTypeException;
 import node.expression.type.NumeralType;
+import node.expression.type.StringType;
 import node.primary.AbstractPrimaryNode;
+import node.primary.FloatNode;
+import node.primary.IntegerNode;
+import node.primary.StringNode;
+import visitor.semantic.typecast.ExpressionCastVisitor;
+import visitor.semantic.typecast.TypeCaster;
+
+import java.util.ArrayList;
 
 /**
  * Returns the type of a given expression.
  */
 public class ExpressionTypeVisitor extends PrimaryVisitor {
 
-    public RootNode visit(AbstractInfixAdditiveNode node) throws SemanticException {
-        isNumeral(node);
-        return node;
-    }
-    public RootNode visit(AbstractInfixConditionalNode node) throws SemanticException {
-        isBoolean(node);
-        return node;
-    }
-    public RootNode visit(AbstractInfixMultiplicativeNode node) throws SemanticException {
-        isNumeral(node);
-        return node;
+    private static ArrayList<Class> types = new ArrayList<>();
+    static {
+        types.add(StringNode.class);
+        types.add(FloatNode.class);
+        types.add(IntegerNode.class);
     }
 
-    public RootNode visit(AbstractInfixRelationNode node) throws SemanticException {
-        isNumeral(node);
-        return node;
+    public RootNode visit(AbstractInfixExpressionNode node) throws SemanticException {
+        RootNode leftType = visit(node.getLeft());
+        RootNode rightType = visit(node.getRight());
+        return highestOrder(leftType, rightType);
     }
     public RootNode visit(ListNode node) throws IllegalTypeException {
         RootNode firstElement = node.getFirstElement();
@@ -44,7 +48,6 @@ public class ExpressionTypeVisitor extends PrimaryVisitor {
 
         return node;
     }
-
     // Visitor used to ensure that all elements is the same type as the first element in a ListNode
     public void visit(RootNode element, RootNode firstElement) throws IllegalTypeException {
         if (element != null) {
@@ -58,20 +61,11 @@ public class ExpressionTypeVisitor extends PrimaryVisitor {
             } else throw new IllegalTypeException("Types in list are not the same!");
         }
     }
-    protected void isNumeral(AbstractInfixExpressionNode node) throws SemanticException {
-        if(!(visit(node.getLeft()) instanceof NumeralType))
-            throw new IllegalTypeException(node.getLeft().getLine()+" Illegal type: "+node.getLeft().toString()+
-                    " for type "+node.toString());
-        else if(!(visit(node.getRight()) instanceof NumeralType))
-            throw new IllegalTypeException(node.getRight().getLine()+" Illegal type: "+node.getRight().toString()+
-                    " for type "+node.toString());
-    }
-    protected void isBoolean(AbstractInfixExpressionNode node) throws SemanticException {
-        if(!(visit(node.getLeft()) instanceof BooleanType))
-            throw new IllegalTypeException(node.getLeft().getLine()+" Illegal type: "+node.getLeft().toString()+
-                    " for type "+node.getClass().getSimpleName());
-        else if(!(visit(node.getRight()) instanceof BooleanType))
-            throw new IllegalTypeException(node.getRight().getLine()+" Illegal type: "+node.getRight().toString()+
-                    " for type "+node.toString());
+    private RootNode highestOrder(RootNode left, RootNode right) throws SemanticException {
+        if(types.indexOf(left.getClass()) > types.indexOf(right.getClass()))
+            return TypeCaster.cast(right, left);
+        else if (types.indexOf(right.getClass()) > types.indexOf(left.getClass()))
+            return TypeCaster.cast(left, right);
+        return left;
     }
 }

0bcf5e93aff6f46cef0f00af2d6b591f64a654f5
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-05-10 12:48:31 +0200

    Add template class.

---
+++ src/node/scope/FunctionTemplate.java
@@ -0,0 +1,9 @@
+package node.scope;
+
+import gen.Ardu3kParser;
+
+public class FunctionTemplate extends FunctionNode {
+    public FunctionTemplate(Ardu3kParser.FunctionContext ctx) {
+        super(ctx);
+    }
+}

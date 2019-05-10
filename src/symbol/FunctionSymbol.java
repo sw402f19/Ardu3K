24855f0ed535d39a8881dfa56bf4ddd29b8fc6fa
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-05-10 12:49:27 +0200

    Function specifically for function nodes or maybe templates.

---
+++ src/symbol/FunctionSymbol.java
@@ -0,0 +1,59 @@
+package symbol;
+
+import com.rits.cloning.Cloner;
+import node.RootNode;
+import node.scope.FunctionNode;
+import node.statement.FunctionStmtNode;
+
+import java.util.ArrayList;
+
+public class FunctionSymbol extends Symbol {
+
+    private ArrayList<FunctionNode> impls = new ArrayList<>();
+
+    public FunctionSymbol(RootNode name, RootNode type, int depth) {
+        super(name, type, depth);
+    }
+
+    public FunctionNode getImpl(FunctionStmtNode node) {
+        FunctionNode impl = null;
+
+        for(FunctionNode n : impls) {
+            for(int i = 0; i < node.getArguments().children.size(); i++) {
+                if (!node.getArguments().children.get(i).getClass()
+                        .isInstance(n.getParameter().children.get(i)))
+                    break;
+                if(i == node.getArguments().children.size() -1)
+                    impl = n;
+            }
+        }
+        return impl;
+    }
+    public boolean containsImpl(FunctionStmtNode node) {
+        boolean contains = false;
+
+        for(FunctionNode n : impls) {
+            for(int i = 0; i < node.getArguments().children.size(); i++) {
+                if (!node.getArguments().children.get(i).getClass()
+                        .isInstance(n.getParameter().children.get(i)))
+                    break;
+                if(i == node.getArguments().children.size() -1)
+                    contains = true;
+            }
+        }
+        return contains;
+    }
+
+    public void addImpl(FunctionStmtNode node) {
+        Cloner cloner = new Cloner();
+        FunctionNode nodeToAdd = new FunctionNode();
+        FunctionNode template = (FunctionNode) getType();
+
+        FunctionNode type = (FunctionNode) this.getType();
+        if(!containsImpl(node)) {
+            nodeToAdd.setBlock(cloner.deepClone(template.getBlock()));
+            nodeToAdd.setId(cloner.deepClone(template.getId()));
+
+        }
+    }
+}

9e14ec56d24ad73e58fcec536372ea3d2e66559c
kristian <kh.mikkelsen@hotmail.com>
2019-05-11 22:40:36 +0200

    Parameters have local array of types, which index corresponds to their children.

--- src/node/scope/ParameterNode.java
+++ src/node/scope/ParameterNode.java
@@ -5,7 +5,11 @@ import node.RootNode;
 import visitor.ASTVisitor;
 import visitor.BaseASTVisitor;
 
+import java.util.ArrayList;
+
 public class ParameterNode extends RootNode {
+
+    public ArrayList<RootNode> types = new ArrayList<>();
 
     public ParameterNode() {
     }

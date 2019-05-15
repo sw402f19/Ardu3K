000f7c1ccddc881ff73b06268f1ba133f2b82c9e
gamped <mathiasgam@gmail.com>
2019-05-15 11:47:22 +0200

    Added custom pin mode function

--- src/visitor/codegen/Ardu3K_CustomCode.cpp
+++ src/visitor/codegen/Ardu3K_CustomCode.cpp
@@ -5,25 +5,55 @@
 */
 class Ardu3K {
 public:
+    // Return true if it is after or equal to the wait time
     static bool AfterCheck(long startTime, long waitTime) {
         long desiredTime = startTime + waitTime;
 
-        if (desiredTime < millis()){
+        if (desiredTime <= millis()){
             return true;
         } else return false;
     }
 
+    // Return true if it is before or equal to the wait time
     static bool BeforeCheck(long startTime, long waitTime) {
         long desiredTime = startTime + waitTime;
 
-        if (desiredTime > millis()){
+        if (desiredTime => millis()){
             return true;
         } else return false;
     }
 
-    static void ResetTimer(long *timer){
-        *timer = millis();
-    }
+    // "Reset" the timer by seting it to current time
+    static void ResetTimer(long *timer) { *timer = millis(); }
+
+    // Sets the bitflag representing the data direction of the pin
+    static void SetPinMode(short index, bool bAnalog, bool bOutput){
+        if (bAnalog) {
+            if (index >= 0 && index <= 5) {
+                if (bOutput) {
+                    DDRC |= (1 << index);
+                } else {
+                    DDRC &= ~(1 << index); 
+                }
+            }
+        } else {
+            if (index >= 0 && index <= 13) {
+                if (bOutput) {
+                    if (index <= 7) {
+                        DDRD |= (1 << index);
+                    } else { 
+                        DDRB |= (1 << (index - 8));
+                    }
+                } else {
+                    if (index <= 7) {
+                        DDRD &= ~(1 << index);
+                    } else { 
+                        DDRB &= ~(1 << (index - 8));
+                    }
+                }
+            }
+        }
+    } 
 
     // Read function following the format of the other pin functions
     static int PinRead(short index, bool bAnalog) {
@@ -57,7 +87,7 @@ public:
         if (bAnalog) {
             if (index >= 0 && index <= 5) {
                 if (bWriteVal) {
-                    PORTC &= (1 << index);
+                    PORTC |= (1 << index);
                 } else {
                     PORTC &= ~(1 << index); 
                 }
@@ -66,9 +96,9 @@ public:
             if (index >= 0 && index <= 13) {
                 if (bWriteVal) {
                     if (index <= 7) {
-                        PORTD &= (1 << index);
+                        PORTD |= (1 << index);
                     } else { 
-                        PORTB &= (1 << (index - 8));
+                        PORTB |= (1 << (index - 8));
                     }
                 } else {
                     if (index <= 7) {

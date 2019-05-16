23d3342432942604a2f6047255aa7f47484b955e
khmikkelsen <kh.mikkelsen@hotmail.com>
2019-05-16 14:25:11 +0200

    Context constructors.

--- src/node/statement/time/BeforeNode.java
+++ src/node/statement/time/BeforeNode.java
@@ -1,6 +1,12 @@
 package node.statement.time;
 
+import gen.Ardu3kParser;
+
 public class BeforeNode extends AbstractTimeStmtNode {
+    public BeforeNode(Ardu3kParser.BeforeStmtContext ctx) {
+        super(ctx);
+    }
+
     @Override
     public String toString() {
         return "BEFORE";

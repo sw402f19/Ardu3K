779d5299a2fc67a67b2d641773bbcdda2d96ca0c
gamped <mathiasgam@gmail.com>
2019-05-20 10:53:20 +0200

    Created base test structure

---
+++ test/testVisitors/BuilderTest.java
@@ -0,0 +1,11 @@
+package testVisitors;
+
+import node.RootNode;
+
+public class builderTest {
+
+    // The starting point of the test
+    public builderTest(RootNode ast){
+
+    }
+}
---
+++ test/testVisitors/CodeGenTest.java
@@ -0,0 +1,11 @@
+package testVisitors;
+
+import node.RootNode;
+
+public class codeGenTest {
+
+    // The starting point of the test
+    public codeGenTest(String fileName){
+
+    }
+}
